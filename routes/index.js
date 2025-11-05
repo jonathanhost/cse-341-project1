fc65bfe2aaa75260875a9c5491e805c7e3717b3f
jonathan <johh.almeida@gmail.com>
2025-11-05 16:26:58 -0300

    database week 1 finished

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,14 @@
 const router = require('express').Router();
-router.get('/',(req, res) => {res.send('Hello World');});
+
+// Importa o router de contacts
+const contactsRoutes = require('./contacts');
+
+// Rotas
+router.use('/contacts', contactsRoutes);
+
+// Rota raiz (opcional)
+router.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 module.exports = router;
